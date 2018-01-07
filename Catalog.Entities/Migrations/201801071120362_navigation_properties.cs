ecaac9c393174deb10bd7d93f2e2900d86ec4b0f
Faraz <af.ahmfrz@gmail.com>
2018-01-07 16:51:14 +0530

    ADD missing migrations from last commit

---
+++ Catalog.Entities/Migrations/201801071120362_navigation_properties.cs
@@ -0,0 +1,16 @@
+namespace Catalog.Entities.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class navigation_properties : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
